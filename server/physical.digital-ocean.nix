23f354e874bee5e0c1bb9da80ec436eee60f50a6
3noch <eacameron@gmail.com>
2017-01-13 01:39:09 -0500

    Don't keep DigitalOcean keys around

--- server/physical.digital-ocean.nix
+++ server/physical.digital-ocean.nix
@@ -1,13 +1,12 @@
 with import ./common.nix;
 
-{ apiAuthToken, dropletRegion, dropletSize }: {
+{ dropletRegion, dropletSize }: {
   resources.sshKeyPairs.ssh-key = {};
 
   ${machineName} = {...}: {
     deployment = {
       targetEnv = "digitalOcean";
       digitalOcean = {
-        authToken = apiAuthToken;
         region    = dropletRegion;
         size      = dropletSize;
       };
--- server/physical.prod.nix
+++ server/physical.prod.nix
@@ -1,5 +1,4 @@
 import ./physical.digital-ocean.nix {
-  apiAuthToken  = (import ./digital-ocean.keys.nix).apiAuthToken;
   dropletRegion = "nyc1";   # https://developers.digitalocean.com/documentation/v2/#list-all-regions
   dropletSize   = "512mb";  # https://developers.digitalocean.com/documentation/v2/#list-all-sizes
 }
