ec532dc500d6f060a6732ac69792cc05d83ecfd9
3noch <eacameron@gmail.com>
2017-01-28 15:52:48 -0500

    Update Project:M36

--- project-m36.nix
+++ project-m36.nix
@@ -3,8 +3,8 @@ let
   project-m36-nix = fetchFromGitHub {
     owner  = "3noch";
     repo   = "project-m36";
-    rev    = "d8c338507ce0ccd203cc0487847142685f9642bd";
-    sha256 = "1a937kz3i70k8rr1cqycnxpzwp7n2jl9p7ws9fh3gqzwza4jjrl5";
+    rev    = "b3fbf7bb8f2194f43f0da7361d2d67f0fbb5b9d8";
+    sha256 = "1kmk8c50hinjkmw3vapy740p7ps7g7m7cgvmvh3n055k7384sc96";
   };
 in {
   src = project-m36-nix;
