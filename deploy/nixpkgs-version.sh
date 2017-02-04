5a97b8aef0d50d231f8c6588e54cb439c9e27a98
3noch <eacameron@gmail.com>
2017-02-04 01:37:57 -0500

    Refactor; update; deploy to prod

--- deploy/nixpkgs-version.sh
+++ deploy/nixpkgs-version.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 export nixpkgs_channel="https://nixos.org/channels/nixpkgs-unstable"
-export nixpkgs_snapshot="https://d3g5gsiof5omrk.cloudfront.net/nixpkgs/nixpkgs-17.03pre99424.92b1e39/nixexprs.tar.xz"
+export nixpkgs_snapshot="https://d3g5gsiof5omrk.cloudfront.net/nixpkgs/nixpkgs-17.03pre100133.a14217e/nixexprs.tar.xz"
 
 if [ "$(uname)" == "Darwin" ]; then
-  export nixops_version="/nix/store/88xr39q34mvzbniv7wlhydjmnwn8lgsm-nixops-1.5pre2121_fc43d9c"
+  export nixops_version="/nix/store/jcvvin1i87vin4hwldz0b68sd64n0ly0-nixops-1.5pre2127_c3b164a"
 else
-  export nixops_version="/nix/store/d70851jdygz5mcdwrxypx7wb2ijvw509-nixops-1.5pre2121_fc43d9c"
+  export nixops_version="/nix/store/axd7sljp4hyrx9qc78m082d7v8nqmxrx-nixops-1.5pre2127_c3b164a"
 fi
 
 if [ ! -d "$nixops_version" ]; then
--- project-m36.nix
+++ project-m36.nix
@@ -1,14 +1,24 @@
-{ callPackage, fetchFromGitHub, haskell, ... }:
+{ fetchFromGitHub, haskell, haskellPackages, ... }:
 let
-  project-m36-nix = fetchFromGitHub {
-    owner  = "3noch";
+
+  project-m36-src = fetchFromGitHub {
+    owner  = "agentm";
     repo   = "project-m36";
-    rev    = "bf783b65577a7c40305731edc3326f0caa7ef29b";
-    sha256 = "10vnqay7kyw54a0lmmzfcrxr2z6jcj2gbdcrsqr4q6lqgd0wjz7w";
+    rev    = "9cfdd630d81ef88c9c9db5def386058fb56af9c5";
+    sha256 = "1gzhw9p88c5zld9qh0p199xf046k5ra7ylaygvjis30scy8kggjl";
+  };
+
+  haskellPkgs = haskellPackages.override {
+    overrides = self: super: {
+      distributed-process-client-server = haskell.lib.dontCheck (haskell.lib.doJailbreak super.distributed-process-client-server);
+      distributed-process-extras = haskell.lib.doJailbreak super.distributed-process-extras;
+      megaparsec = self.callHackage "megaparsec" "4.4.0" {};
+      persistent = super.persistent_2_2_4_1;
+      persistent-template = super.persistent-template_2_1_8_1;
+      project-m36 = haskell.lib.dontHaddock (haskell.lib.dontCheck (self.callCabal2nix project-m36-src {}));
+    };
   };
 in {
-  src = project-m36-nix;
-  bin = callPackage "${project-m36-nix}/default.nix" {
-    haskellPackages = haskell.packages.ghc802;
-  };
+  src = project-m36-src;
+  bin = haskellPkgs.project-m36;
 }
